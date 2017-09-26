150124d4e14ae855330a321066eb819e8c379017
Guosiying <602036484@qq.com>
2017-09-26 20:50:59 +0800

    add identity information

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ public class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello!");
 		System.out.println("I am GuoSiying.");
+                System.out.println("I am a student of Tianjin University.");
 	}
 }
